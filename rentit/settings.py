9c085e2b80a81f69a38873fad420ec9047041c02
ABDUL SALIM <66547556+Abdul-Salim@users.noreply.github.com>
2020-09-29 19:28:37 +0530

    Update settings.py

--- rentit/settings.py
+++ rentit/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '25*3dcz*g2t&&)-+5uj@hsc#0-!@^p*x7#uy!hgf66z(8bodlq'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -122,7 +121,7 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS =True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'rentitapp2021@gmail.com' #'rentitapp2021@gmail.com'
-EMAIL_HOST_PASSWORD = 'swab@123' #'swab@rentit'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 EMAIL_PORT = 587
 
 # Static files (CSS, JavaScfript, Images)
